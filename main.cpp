1212cbfe16fc0b790dc792b42cf5bcd45cfc5c4b
Johan Hugg <karljohanhugg@gmail.com>
2014-11-04 18:24:48 +0100

    Lighting tweak

--- main.cpp
+++ main.cpp
@@ -762,9 +762,9 @@ void render() {
 				x+lx, 1.0f,  z+lz,
 				0.0f, 1.0f,  0.0f);
 
-	addLights();
+	//addLights();
 	//addMtrl();
-	addTeapot();
+	//addTeapot();
 
 	glTranslatef(-2, -5, -20);
 	
