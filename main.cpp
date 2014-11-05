eac9fd954c9929f07c95a5f8e9ed448097a1bae5
ntn11phm <ntn11phm@student.hig.se>
2014-11-05 18:57:02 +0100

    started a normalVector swapper

--- main.cpp
+++ main.cpp
@@ -42,9 +42,27 @@ float teapotAngle = 0.0;
 
 GLfloat normalVectors[6][3] = {
 		{ 0, 0, 1 }, // front
-		{ 0, 0,-1 }, // back
+		{ 0, 0, -1 }, // back
 		{ 1, 0, 0 }, // left
-		{-1, 0, 0 }, // right
+		{ -1, 0, 0 }, // right
+		{ 0, 1, 0 }, // top
+		{ 0, -1, 0 } // bottom
+};
+
+GLfloat normalVectorsInnerWalls[6][3] = {
+		{ 0, 0,-1 }, // front
+		{ 0, 0, 1 }, // back
+		{-1, 0, 0 }, // left
+		{ 1, 0, 0 }, // right
+		{ 0,-1, 0 }, // top
+		{ 0, 1, 0 } // bottom
+};
+
+GLfloat normalVectorsOuterWalls[6][3] = {
+		{ 0, 0, 1 }, // front
+		{ 0, 0, -1 }, // back
+		{ 1, 0, 0 }, // left
+		{ -1, 0, 0 }, // right
 		{ 0, 1, 0 }, // top
 		{ 0, -1, 0 } // bottom
 };
@@ -277,6 +295,80 @@ void drawCube(float endPoint[3], int sideSelector[6], float *texture_container) 
 		glPushMatrix(); // ground
 		glRotatef(270, 1.0, 0.0, 0.0);
 		drawQuad_XY(roofgroundarr, texture_container[0], normalVectors[5]);
+		glPopMatrix();
+	}
+	glPopMatrix();
+
+}
+
+void drawCube(float endPoint[3], int sideSelector[6], float *texture_container, bool isOuterWall) {
+
+	float frontbackarr[4][3] = {
+			{ 0.0, 0.0, 0.0 },
+			{ endPoint[0], 0.0, 0.0 },
+			{ 0.0, endPoint[1], 0.0 },
+			{ endPoint[0], endPoint[1], 0.0 }
+	};
+
+	float leftrightarr[4][3] = {
+			{ 0.0, 0.0, 0.0 },
+			{ endPoint[2], 0.0, 0.0 },
+			{ 0.0, endPoint[1], 0.0 },
+			{ endPoint[2], endPoint[1], 0.0 }
+	};
+
+	float roofgroundarr[4][3] = {
+			{ 0.0, 0.0, 0.0 },
+			{ endPoint[0], 0.0, 0.0 },
+			{ 0.0, endPoint[2], 0.0 },
+			{ endPoint[0], endPoint[2], 0.0 }
+	};
+
+	GLfloat normVect[6][3];
+
+	if (isOuterWall)
+		GLfloat *normVect = *normalVectorsOuterWalls;
+	else
+		GLfloat *normVect = *normalVectorsInnerWalls;
+
+
+	glPushMatrix();
+	if (sideSelector[0] == 1) {
+		glPushMatrix(); // Back
+		glTranslatef(0.0, 0.0, -endPoint[2]);
+		drawQuad_XY(frontbackarr, texture_container[1], normVect[1]);
+		glPopMatrix();
+	}
+	if (sideSelector[1] == 1) {
+		glPushMatrix(); // front
+		glTranslatef(0.0, 0.0, 0.0);
+		drawQuad_XY(frontbackarr, texture_container[1], normVect[0]);
+		glPopMatrix();
+	}
+	if (sideSelector[2] == 1) {
+		glPushMatrix(); // left
+		glRotatef(90, 0.0, 1.0, 0.0);
+		drawQuad_XY(leftrightarr, texture_container[1], normVect[3]);
+		glPopMatrix();
+	}
+	if (sideSelector[3] == 1) {
+		glPushMatrix(); // right
+		glRotatef(90, 0.0, 1.0, 0.0);
+		glTranslatef(0.0, 0.0, endPoint[0]);
+		drawQuad_XY(leftrightarr, texture_container[1], normVect[3]);
+		glPopMatrix();
+	}
+	if (sideSelector[4] == 1) {
+		glPushMatrix(); // roof
+		glRotatef(270, 1.0, 0.0, 0.0);
+		glTranslatef(0.0, 0.0, endPoint[1]);
+		drawQuad_XY(roofgroundarr, texture_container[2], normVect[4]);
+		glPopMatrix();
+	}
+	if (sideSelector[5] == 1) {
+		glPushMatrix(); // ground
+		glRotatef(270, 1.0, 0.0, 0.0);
+		drawQuad_XY(roofgroundarr, texture_container[0], normVect[5]);
 		glPopMatrix();
 	}
 	glPopMatrix();
